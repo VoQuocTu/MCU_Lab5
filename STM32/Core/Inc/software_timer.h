c4132472d6175921b8bcbd54c4ef7492fc9cf255
VoQuocTu <tu.vobk22@hcmut.edu.vn>
2025-11-26 08:44:27 +0700

    update

---
+++ STM32/Core/Inc/software_timer.h
@@ -0,0 +1,16 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Nov 19, 2025
+ *      Author: HP
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+#include "global.h"
+
+void setTimer(int duration);
+void timerRun(void);
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
